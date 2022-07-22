b46172ba30724027b2fdb6d2a3ad8d0c40445d40
Ozan <ozan.yasin97@gmail.com>
2022-07-22 17:44:48 +0300

    homework-4 error-page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Links from './Links';
-import './App.css';
 
 export default function App() {
   return (
--- src/Links.js
+++ src/Links.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DogList from './DogList';
 import DogDetails from './DogDetails';
+// import ErrorPage from './ErrorPage';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 export default function Links() {
@@ -9,8 +10,7 @@ export default function Links() {
       <Route path="/dogs" element={<DogList />} />
       <Route path="/dogs/:dogName" element={<DogDetails />} />
       <Route path="*" element={<Navigate to="/dogs" />} />
+      {/* <Route path="*" element={<ErrorPage />} /> */}
     </Routes>
   );
 }
-
-// <Route path="*" element={<ErrorPage />} />
